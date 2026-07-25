8b6d068268eb2316bcac77511258b2e0ed683c6b
でみぐらすそーせーじ <quesbang@gmail.com>
2026-07-25 21:29:58 +0900

    Update app.js

--- app.js
+++ app.js
@@ -19,6 +19,8 @@ const ENEMIES = {
   lich:{name:"リッチ",baseHp:2200,baseAttack:110,baseDefense:70,baseExp:2500,gold:2500,desc:"死を超越した魔術師。"},
   kraken:{name:"クラーケン",baseHp:3500,baseAttack:150,baseDefense:90,baseExp:4500,gold:5000,desc:"海を支配する怪物。"},
   void_beast:{name:"虚無獣",baseHp:6000,baseAttack:220,baseDefense:130,baseExp:9000,gold:10000,desc:"世界の外側から来た怪物。"}
+　kishi: {name: "方向の騎士",hp: 10000,attack: 99999999,defense: 1000,exp: 15000,gold: 15000,desc:"こいつはどこから来たのだろうか．．．"},
+　funny: {name: "大草原不可避",hp: 10,attack: 1000,defense: 1000000,exp: 15000,gold: 15000,desc:"こいつはたぶん...ネットから来たな"},
 };
 
 /* =========================================
